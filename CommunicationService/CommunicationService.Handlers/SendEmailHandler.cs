b30895366559bb205131b9f352153bf42e8e2d91
Jawwad Mukhtar <jawwad.mukhtar@gmail.com>
2020-04-17 18:47:53 +0100

    read values successfully in handler

--- CommunicationService/CommunicationService.Handlers/SendEmailHandler.cs
+++ CommunicationService/CommunicationService.Handlers/SendEmailHandler.cs
@@ -1,6 +1,8 @@
-﻿using CommunicationService.Core.Domains.Entities;
+﻿using CommunicationService.Core.Configuration;
+using CommunicationService.Core.Domains.Entities;
 using CommunicationService.Core.Interfaces.Repositories;
 using MediatR;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,10 +14,13 @@ namespace CommunicationService.Handlers
     public class SendEmailHandler : IRequestHandler<SendEmailRequest>
     {
         private readonly IRepository _repository;
+        private readonly IOptions<SendGridConfig> _sendGridConfig;
 
-        public SendEmailHandler(IRepository repository)
+        public SendEmailHandler(IRepository repository, IOptions<SendGridConfig> sendGridConfig)
         {
             _repository = repository;
+            _sendGridConfig = sendGridConfig;
+            string a =_sendGridConfig.Value.ApiKey;
         }
 
         public Task<Unit> Handle(SendEmailRequest request, CancellationToken cancellationToken)
