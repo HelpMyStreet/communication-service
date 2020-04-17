e50aff06ea4f39d5e1366fa5e62c45313ab170e6
Jawwad Mukhtar <jawwad.mukhtar@gmail.com>
2020-04-17 19:12:06 +0100

    remove config reference from handler

--- CommunicationService/CommunicationService.Handlers/SendEmailHandler.cs
+++ CommunicationService/CommunicationService.Handlers/SendEmailHandler.cs
@@ -1,12 +1,7 @@
-﻿using CommunicationService.Core.Configuration;
-using CommunicationService.Core.Domains.Entities;
+﻿using CommunicationService.Core.Domains.Entities;
 using CommunicationService.Core.Interfaces.Repositories;
 using CommunicationService.Core.Interfaces.Services;
 using MediatR;
-using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,16 +9,13 @@ namespace CommunicationService.Handlers
 {
     public class SendEmailHandler : IRequestHandler<SendEmailRequest>
     {
-        private readonly IRepository _repository;
-        private readonly IOptions<SendGridConfig> _sendGridConfig;
+        private readonly IRepository _repository;        
         private readonly ISendEmailService _sendEmailService;
 
-        public SendEmailHandler(IRepository repository, IOptions<SendGridConfig> sendGridConfig, ISendEmailService sendEmailService)
+        public SendEmailHandler(IRepository repository, ISendEmailService sendEmailService)
         {
-            _repository = repository;
-            _sendGridConfig = sendGridConfig;
-            _sendEmailService = sendEmailService;
-            string a =_sendGridConfig.Value.ApiKey;
+            _repository = repository;            
+            _sendEmailService = sendEmailService;            
         }
 
         public async Task<Unit> Handle(SendEmailRequest request, CancellationToken cancellationToken)
