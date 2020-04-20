e553b6ce56b1620a34e7a0a1ddca20fa266e4bf0
Usmaan Razaq <usrazaq@gmail.com>
2020-04-20 16:29:43 +0100

    added startup config and added http client factory utility

---
+++ CommunicationService/CommunicationService.Core/Utils/HttpClientWrapper.cs
@@ -0,0 +1,36 @@
+﻿using CommunicationService.Core.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommunicationService.Core.Utils
+{
+    public class HttpClientWrapper : IHttpClientWrapper
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HttpClientWrapper(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public Task<HttpResponseMessage> GetAsync(HttpClientConfigName httpClientConfigName, string absolutePath, CancellationToken cancellationToken)
+        {
+            HttpClient httpClient = _httpClientFactory.CreateClient(httpClientConfigName.ToString());
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, absolutePath);
+            return httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+        }
+
+        public Task PostAsync(HttpClientConfigName httpClientConfigName, string absolutePath, HttpContent content, CancellationToken cancellationToken)
+        {
+            HttpClient httpClient = _httpClientFactory.CreateClient(httpClientConfigName.ToString());
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, absolutePath);
+            request.Content = content;
+            return httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+        }
+
+    }
+}
---
+++ CommunicationService/CommunicationService.Core/Utils/IHttpClientWrapper.cs
@@ -0,0 +1,16 @@
+﻿using CommunicationService.Core.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommunicationService.Core.Utils
+{
+    public interface IHttpClientWrapper
+    {
+        Task<HttpResponseMessage> GetAsync(HttpClientConfigName httpClientConfigName, string absolutePath, CancellationToken cancellationToken);
+        Task PostAsync(HttpClientConfigName httpClientConfigName, string absolutePath, HttpContent content, CancellationToken cancellationToken);
+    }
+}
