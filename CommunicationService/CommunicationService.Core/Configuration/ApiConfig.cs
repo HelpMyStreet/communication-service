28651dbf2d0805186c529cbf84829d7053290561
Usmaan Razaq <usrazaq@gmail.com>
2020-04-20 17:02:08 +0100

    removed key from config

--- CommunicationService/CommunicationService.Core/Configuration/ApiConfig.cs
+++ CommunicationService/CommunicationService.Core/Configuration/ApiConfig.cs
@@ -7,9 +7,7 @@ namespace CommunicationService.Core.Configuration
     public class ApiConfig
     {
         public string BaseAddress { get; set; }
-
-        public string Key { get; set; }
-
+        
         public Dictionary<string, string> Headers { get; set; } = new Dictionary<string, string>();
 
         public TimeSpan? Timeout { get; set; }
