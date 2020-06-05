9c32460180c90255b02148fc5699ad6bdc10fe40
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-06-05 13:05:25 +0100

    save changes

---
+++ CommunicationService/CommunicationService.AzureFunction/SendGridWebHook.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using MediatR;
+using System;
+using System.Net;
+using AzureFunctions.Extensions.Swashbuckle.Attribute;
+using HelpMyStreet.Contracts.CommunicationService.Response;
+using HelpMyStreet.Contracts.CommunicationService.Request;
+using HelpMyStreet.Contracts.Shared;
+using Microsoft.AspNetCore.Http;
+using CommunicationService.Core.Interfaces.Repositories;
+using CommunicationService.Core.Domains;
+using Newtonsoft.Json;
+using System.IO;
+using System.Dynamic;
+
+namespace CommunicationService.AzureFunction
+{
+    public class SendGridWebHook
+    {
+        private readonly ICosmosDbService _cosmosDbService;
+
+        public SendGridWebHook(ICosmosDbService cosmosDbService)
+        {
+            _cosmosDbService = cosmosDbService;
+        }
+
+        [FunctionName("SendGridWebHook")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                log.LogInformation("C# HTTP trigger function processed a request.");
+
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                dynamic data  = JsonConvert.DeserializeObject<ExpandoObject>(requestBody);
+                data.id = Guid.NewGuid();
+                await _cosmosDbService.AddItemAsync(data);
+                return new OkObjectResult(true);
+            }
+            catch (Exception exc)
+            {
+                log.LogError("Exception occured in SendGridWebHook", exc);
+                return new BadRequestObjectResult(exc);
+            }
+        }
+    }
+}
---
+++ CommunicationService/CommunicationService.Core/Interfaces/Repositories/ICosmosDbService.cs
@@ -0,0 +1,15 @@
+﻿using CommunicationService.Core.Domains;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationService.Core.Interfaces.Repositories
+{
+    public interface ICosmosDbService
+    {
+        Task<IEnumerable<object>> GetItemsAsync(string query);
+        Task<object> GetItemAsync(string id);
+        Task AddItemAsync(dynamic item);
+    }
+}
