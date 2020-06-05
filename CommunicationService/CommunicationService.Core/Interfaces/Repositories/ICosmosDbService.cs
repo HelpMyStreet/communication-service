9b2021827aa6a0480331b2b0aae172e54fe54582
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-06-05 13:17:21 +0100

    remove unused using

--- CommunicationService/CommunicationService.Core/Interfaces/Repositories/ICosmosDbService.cs
+++ CommunicationService/CommunicationService.Core/Interfaces/Repositories/ICosmosDbService.cs
@@ -1,7 +1,4 @@
-﻿using CommunicationService.Core.Domains;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace CommunicationService.Core.Interfaces.Repositories
