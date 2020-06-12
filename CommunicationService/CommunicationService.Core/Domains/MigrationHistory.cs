1a70894d113d70dc67ec173e81f6e5d4b1cedf1b
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-06-12 14:59:58 +0100

    create console app

---
+++ CommunicationService/CommunicationService.Core/Domains/MigrationHistory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunicationService.Core.Domains
+{
+    public class MigrationHistory
+    {
+        public string MigrationId { get; set; }
+        public string id { get; set; }
+    }
+}
---
+++ CommunicationService/CommunicationService.SendGridManagement/Program.cs
@@ -0,0 +1,39 @@
+﻿using CommunicationService.Core.Domains;
+using CommunicationService.Core.Interfaces.Repositories;
+using CommunicationService.Repo;
+using CommunicationService.SendGridManagement.Configuration;
+using CommunicationService.SendGridManagement.Models;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunicationService.SendGridManagement
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            string currentDirectory = Environment.CurrentDirectory;
+            IConfigurationRoot config = new ConfigurationBuilder()
+               .SetBasePath(currentDirectory)
+               .AddJsonFile("appsettings.json", true)
+               .Build();
+
+            CosmosConfig cosmosConfig = config.GetSection("CosmosConfig").Get<CosmosConfig>();
+            SendGridConfig sendGridConfig = config.GetSection("SendGridConfig").Get<SendGridConfig>();
+
+            EmailTemplateUploader emailTemplateUploader = new EmailTemplateUploader(
+                cosmosConfig,
+                sendGridConfig,
+                currentDirectory
+                );
+
+            await emailTemplateUploader.Migrate();
+            int i = 1;
+        }
+    }
+}
