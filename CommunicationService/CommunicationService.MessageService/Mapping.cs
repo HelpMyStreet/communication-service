5a49d87def7579066d6870ac08da4c3e4fa18328
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-06-15 08:48:04 +0100

    Rename SendGridWebHookImproved

---
+++ CommunicationService/CommunicationService.MessageService/Mapping.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunicationService.MessageService
+{
+    public static class Mapping
+    {
+        public static Dictionary<HelpMyStreet.Utils.Enums.SupportActivities, string> ActivityMappings = new Dictionary<HelpMyStreet.Utils.Enums.SupportActivities, string>() {
+            { HelpMyStreet.Utils.Enums.SupportActivities.Shopping, "Shopping" },
+            { HelpMyStreet.Utils.Enums.SupportActivities.CollectingPrescriptions, "Prescriptions" },
+            { HelpMyStreet.Utils.Enums.SupportActivities.Errands, "Errands" },
+            { HelpMyStreet.Utils.Enums.SupportActivities.DogWalking, "Dog Walking" },
+            { HelpMyStreet.Utils.Enums.SupportActivities.MealPreparation, "Prepared Meal" },
+            { HelpMyStreet.Utils.Enums.SupportActivities.PhoneCalls_Friendly, "Friendly Chat" },
+            { HelpMyStreet.Utils.Enums.SupportActivities.PhoneCalls_Anxious, "Supportive Chat" },
+            { HelpMyStreet.Utils.Enums.SupportActivities.HomeworkSupport, "Homework" },
+            { HelpMyStreet.Utils.Enums.SupportActivities.CheckingIn, "Check In" },
+            { HelpMyStreet.Utils.Enums.SupportActivities.FaceMask, "Face Covering" },
+            { HelpMyStreet.Utils.Enums.SupportActivities.WellbeingPackage, "Wellbeing Package" },
+            { HelpMyStreet.Utils.Enums.SupportActivities.Other, "Other" }
+        };
+    }
+}
--- CommunicationService/CommunicationService.MessageService/Substitution/NewTaskNotificationData.cs
+++ CommunicationService/CommunicationService.MessageService/Substitution/NewTaskNotificationData.cs
@@ -8,24 +8,31 @@ namespace CommunicationService.MessageService.Substitution
 {
     public class NewTaskNotificationData : BaseDynamicData
     {
-        public int UserID { get; private set; }
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-
+        public string Activity { get; set; }
+        public string PostCode { get; set; }
+        public double DistanceFromPostCode { get; set; }
+        public string DueDate { get; set; }
         public bool IsNotVerified { get; private set; }
-
         public bool IsStreetChampionOfPostcode { get; private set; }
+        public bool IsHealthCritical { get; set; }
 
-        public SupportActivities Activity { get; private set; }
-
-        public NewTaskNotificationData(int userID, string firstName, string lastName,bool isNotVerified, bool isStreetChampionOfPostcode, SupportActivities activity)
+        public NewTaskNotificationData(
+            string activity,
+            string postcode,
+            double distanceFromPostcode,
+            string dueDate,
+            bool isNotVerified,
+            bool isStreetChampionOfPostcode,
+            bool isHealthCritical
+            )
         {
-            UserID = userID;
-            FirstName = firstName;
-            LastName = lastName;
             Activity = activity;
+            PostCode = postcode;
+            DistanceFromPostCode = distanceFromPostcode;
+            DueDate = dueDate;
             IsNotVerified = isNotVerified;
             IsStreetChampionOfPostcode = isStreetChampionOfPostcode;
+            IsHealthCritical = isHealthCritical;
         }
     }
-}
+}    
