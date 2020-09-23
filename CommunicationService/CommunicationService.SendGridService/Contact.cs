2d0819e8c9b319563874af8dde4ec3edea71c88b
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-09-23 10:54:03 +0100

    add marketing add and delete

---
+++ CommunicationService/CommunicationService.SendGridService/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunicationService.SendGridService
+{
+    public class Contact
+    {
+        public string id { get; set; }
+        public string email { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+    }
+}
