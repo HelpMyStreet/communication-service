63092be1c342cf6c3ab094d2c5e7495db6dedc29
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-09-23 10:54:03 +0100

    add marketing add and delete

---
+++ CommunicationService/CommunicationService.Handlers/DeleteMarketingContactHandler.cs
@@ -0,0 +1,23 @@
+﻿using CommunicationService.Core.Interfaces.Services;
+using HelpMyStreet.Contracts.CommunicationService.Request;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommunicationService.Handlers
+{
+    public class DeleteMarketingContactHandler : IRequestHandler<DeleteMarketingContactRequest, bool>
+    {
+        private readonly IConnectSendGridService _connectSendGridService;
+
+        public DeleteMarketingContactHandler(IConnectSendGridService connectSendGridService)
+        {
+            _connectSendGridService = connectSendGridService;
+        }
+
+        public async Task<bool> Handle(DeleteMarketingContactRequest request, CancellationToken cancellationToken)
+        {
+            return await _connectSendGridService.DeleteMarketingContact(request.MarketingContact);
+        }
+    }
+}
--- CommunicationService/CommunicationService.SendGridService/ConnectSendGridService.cs
+++ CommunicationService/CommunicationService.SendGridService/ConnectSendGridService.cs
@@ -3,6 +3,7 @@ using CommunicationService.Core.Domains;
 using CommunicationService.Core.Domains.SendGrid;
 using CommunicationService.Core.Exception;
 using CommunicationService.Core.Interfaces.Services;
+using HelpMyStreet.Contracts.CommunicationService.Request;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using SendGrid;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -25,6 +27,78 @@ namespace CommunicationService.SendGridService
         {
             _sendGridConfig = sendGridConfig;
             _sendGridClient = sendGridClient;
+        }
+
+        public async Task<bool> AddNewMarketingContact(MarketingContact marketingContact)
+        {
+            MarketingContactDetails marketingContactDetails = new MarketingContactDetails()
+            {
+                contacts = new Contact[1]
+                {
+                   new Contact()
+                   {
+                       first_name = marketingContact.FirstName,
+                       last_name = marketingContact.LastName,
+                       email = marketingContact.EmailAddress
+                   }
+                }
+            };
+
+            string requestBody = JsonConvert.SerializeObject(marketingContactDetails);
+
+            Response response = await _sendGridClient.RequestAsync(SendGridClient.Method.PUT, requestBody, null, "marketing/contacts").ConfigureAwait(false);
+
+
+            if (response != null && response.StatusCode == HttpStatusCode.Accepted)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private async Task<string> GetContactId(MarketingContact marketingContact)
+        {
+            string result = string.Empty;
+            Response response = await _sendGridClient.RequestAsync(SendGridClient.Method.GET, null, null, "marketing/contacts").ConfigureAwait(false);
+
+            if (response != null && response.StatusCode == HttpStatusCode.OK)
+            {
+                string body = await response.Body.ReadAsStringAsync().ConfigureAwait(false);
+                AllContactDetails contacts = JsonConvert.DeserializeObject<AllContactDetails>(body);
+
+                if (contacts != null && contacts.contact_count > 0)
+                {
+                    var contactToDelete = contacts.result.Where(x => x.first_name == marketingContact.FirstName
+                    && x.last_name == marketingContact.LastName
+                    && x.email == marketingContact.EmailAddress).FirstOrDefault();
+
+                    if (contactToDelete != null)
+                    {
+                        result = contactToDelete.id;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public async Task<bool> DeleteMarketingContact(MarketingContact marketingContact)
+        {
+            bool result = false;
+            var id = await GetContactId(marketingContact);
+
+            if (!string.IsNullOrEmpty(id))
+            {
+                Response response = await _sendGridClient.RequestAsync(SendGridClient.Method.DELETE, null, null, $"marketing/contacts?ids={id}").ConfigureAwait(false);
+                if (response != null && response.StatusCode == HttpStatusCode.Accepted)
+                {
+                    result = true;
+                }
+            }
+            return result;
         }
 
         public async Task<int> GetGroupId(string groupName)
@@ -92,20 +166,20 @@ namespace CommunicationService.SendGridService
             }
         }
 
-        public async Task<bool> SendDynamicEmail(string messageId,string templateName, string groupName, EmailBuildData emailBuildData)
+        public async Task<bool> SendDynamicEmail(string messageId, string templateName, string groupName, EmailBuildData emailBuildData)
         {
             var template = await GetTemplate(templateName).ConfigureAwait(false);
             int groupId = await GetGroupId(groupName).ConfigureAwait(false);
             Personalization personalization = new Personalization()
             {
                 Tos = new List<EmailAddress>() { new EmailAddress(emailBuildData.EmailToAddress, emailBuildData.EmailToName) },
-                TemplateData = emailBuildData.BaseDynamicData,                
+                TemplateData = emailBuildData.BaseDynamicData,
             };
 
             var eml = new SendGridMessage()
             {
                 From = new EmailAddress(_sendGridConfig.Value.FromEmail, _sendGridConfig.Value.FromName),
-                ReplyTo  = new EmailAddress(_sendGridConfig.Value.ReplyToEmail, _sendGridConfig.Value.ReplyToName),
+                ReplyTo = new EmailAddress(_sendGridConfig.Value.ReplyToEmail, _sendGridConfig.Value.ReplyToName),
                 TemplateId = template.id,
                 Asm = new ASM()
                 {
