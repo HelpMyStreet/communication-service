d84710ff1fbb93be712fd38c25eba5be2d9c9d21
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-09-25 16:30:26 +0100

    first commit of inter user message

--- CommunicationService/CommunicationService.GroupService/ConnectGroupService.cs
+++ CommunicationService/CommunicationService.GroupService/ConnectGroupService.cs
@@ -3,8 +3,11 @@ using HelpMyStreet.Contracts.GroupService.Response;
 using HelpMyStreet.Contracts.RequestService.Response;
 using HelpMyStreet.Contracts.Shared;
 using HelpMyStreet.Utils.Enums;
+using HelpMyStreet.Utils.Models;
 using HelpMyStreet.Utils.Utils;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -31,6 +34,27 @@ namespace CommunicationService.GroupService
                 if (getJobsResponse.HasContent && getJobsResponse.IsSuccessful)
                 {
                     return getJobsResponse.Content;
+                }
+                return null;
+            }
+        }
+
+        public async Task<List<int>> GetGroupMembersForGivenRole(int groupID, GroupRoles groupRoles)
+        {
+            string path = $"/api/GetGroupMemberRoles?GroupId={groupID}&userID=-1";// authorisingUserID
+            string absolutePath = $"{path}";
+            using (HttpResponseMessage response = await _httpClientWrapper.GetAsync(HttpClientConfigName.GroupService, absolutePath, CancellationToken.None).ConfigureAwait(false))
+            {
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                var getGroupMemberRolesResponse = JsonConvert.DeserializeObject<ResponseWrapper<GetGroupMemberRolesResponse, GroupServiceErrorCode>>(jsonResponse);
+                if (getGroupMemberRolesResponse.HasContent && getGroupMemberRolesResponse.IsSuccessful)
+                {
+                    GetGroupMemberRolesResponse resp = getGroupMemberRolesResponse.Content;
+
+                    var users = resp.GroupMemberRoles.Where(x => x.Value.Contains((int)groupRoles))
+                        .Select(x => x.Key).ToList();
+
+                    return users;
                 }
                 return null;
             }
