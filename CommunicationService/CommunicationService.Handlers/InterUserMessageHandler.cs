57ba603261576bb00cc2df289dbe59acf32cae33
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-09-25 16:30:26 +0100

    first commit of inter user message

---
+++ CommunicationService/CommunicationService.Handlers/InterUserMessageHandler.cs
@@ -0,0 +1,209 @@
+﻿using CommunicationService.Core.Domains;
+using CommunicationService.Core.Domains.SendGrid;
+using CommunicationService.Core.Interfaces.Repositories;
+using CommunicationService.Core.Interfaces.Services;
+using CommunicationService.MessageService;
+using HelpMyStreet.Contracts.CommunicationService.Request;
+using HelpMyStreet.Contracts.RequestService.Response;
+using HelpMyStreet.Utils.Enums;
+using MediatR;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using Polly.Caching;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommunicationService.Handlers
+{
+    public class InterUserMessageHandler : IRequestHandler<InterUserMessageRequest, bool>
+    {
+        private readonly IConnectGroupService _connectGroupService;
+        private readonly IConnectUserService _connectUserService;
+        private readonly IQueueClient _queueClient;
+
+
+        public InterUserMessageHandler(IConnectGroupService connectGroupService, IConnectUserService connectUserService, IQueueClient queueClient)
+        {
+            _connectGroupService = connectGroupService;
+            _connectUserService = connectUserService;
+            _queueClient = queueClient;
+        }
+
+
+        private int? GetGroupId(InterUserMessageRequest request)
+        {
+            int? groupId = request.From?.GroupRoleType.GroupId;
+
+            if (request.From?.GroupRoleType != null && request.To?.GroupRoleType != null)
+            {
+                if (request.From.GroupRoleType.GroupId != request.To.GroupRoleType.GroupId)
+                {
+                    throw new Exception("GroupId is not the same for both To and From");
+                }
+            }
+
+            if (groupId.HasValue)
+            {
+                return groupId;
+            }
+            else
+            {
+                return request.To?.GroupRoleType.GroupId;
+            }
+        }
+
+        private async Task<List<int>> IdentifyRecipients(InterUserMessageRequest request)
+        {
+            var result = new List<int>();
+
+            if (request.To == null)
+            {
+                throw new Exception("No recipients to send an email to");
+            }
+
+            if (request.To.UserId.HasValue)
+            {
+                result.Add(request.To.UserId.Value);
+                return result;
+            }
+
+            if (request.To.EmailDetails != null)
+            {
+                return result;
+            }
+
+            if (request.To.GroupRoleType != null)
+            {
+                return await GetPartipantDetailsFromGroup(request.To);
+            }
+
+            throw new Exception("Unable to get recipients");
+        }
+
+        private async Task<string> GetSenderDetails(InterUserMessageRequest request)
+        {
+            if (request.From == null)
+            {
+                throw new Exception("No sender details");
+            }
+
+            if (request.From.UserId.HasValue)
+            {
+                var user = await _connectUserService.GetUserByIdAsync(request.From.UserId.Value);
+
+                if (user != null)
+                {
+                    return user.UserPersonalDetails.FirstName;
+                }
+            }
+            if (request.From.EmailDetails != null)
+            {
+                return request.From.EmailDetails.DisplayName;
+            }
+
+            if (request.To.GroupRoleType != null)
+            {
+                var group = await _connectGroupService.GetGroupResponse(request.To.GroupRoleType.GroupId.Value);
+
+                if (group != null)
+                {
+                    return group.Group.GroupName;
+                }
+
+            }
+            throw new Exception("Unable to get senderDetails");
+        }
+
+        public async Task<bool> Handle(InterUserMessageRequest request, CancellationToken cancellationToken)
+        {
+            //var usersFrom = await GetPartipantDetailsFromGroup(request.From);
+
+            Dictionary<string, string> additionalParameters = new Dictionary<string, string>();
+
+            additionalParameters.Add("SenderMessage", request.Content);
+            additionalParameters.Add("SenderFirstName", await GetSenderDetails(request));
+            additionalParameters.Add("FromRequestorRole", request.From.RequestRoleType.RequestRole.ToString());
+
+            var recipients = await IdentifyRecipients(request);
+
+            if(recipients.Count==0)
+            {
+                //check if displayName + email have been passed in
+                if (request.To.EmailDetails != null)
+                {
+                    additionalParameters.Add("RecipientDisplayName", request.To.EmailDetails.DisplayName);
+                    additionalParameters.Add("RecipientEmailAddress", request.To.EmailDetails.EmailAddress);
+                }
+
+                SendMessageRequest sendMessageRequest = new SendMessageRequest()
+                {
+                    BatchID = Guid.NewGuid(),
+                    TemplateName = TemplateName.InterUserMessage,
+                    RecipientUserID = -1,
+                    JobID = request.JobId,
+                    GroupID = GetGroupId(request),
+                    MessageType = MessageTypes.Email,
+                    CommunicationJobType = CommunicationJobTypes.InterUserMessage,
+                    AdditionalParameters = additionalParameters
+                };
+
+                await AddToMessageQueueAsync(sendMessageRequest);
+            }
+            else
+            {
+                Guid batchId = Guid.NewGuid();
+                foreach(int i in recipients)
+                {
+                    SendMessageRequest sendMessageRequest = new SendMessageRequest()
+                    {
+                        BatchID = batchId,
+                        TemplateName = TemplateName.InterUserMessage,
+                        RecipientUserID = i,
+                        JobID = request.JobId,
+                        GroupID = GetGroupId(request),
+                        MessageType = MessageTypes.Email,
+                        CommunicationJobType = CommunicationJobTypes.InterUserMessage,
+                        AdditionalParameters = additionalParameters
+                    };
+
+                    await AddToMessageQueueAsync(sendMessageRequest);
+                }
+            }
+
+            
+            
+
+            return true;
+        }
+
+        private async Task<List<int>> GetPartipantDetailsFromGroup(MessageParticipant messageParticipant)
+        {
+            if (messageParticipant.GroupRoleType != null && messageParticipant.GroupRoleType.GroupId.HasValue)
+            {
+                var users = await _connectGroupService.GetGroupMembersForGivenRole
+                    (   
+                        messageParticipant.GroupRoleType.GroupId.Value,
+                        messageParticipant.GroupRoleType.GroupRoles
+                     );
+                return users;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private async Task AddToMessageQueueAsync(SendMessageRequest sendMessageRequest)
+        {
+            string messageBody = JsonConvert.SerializeObject(sendMessageRequest);
+            var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+            // Send the message to the queue
+            await _queueClient.SendAsync(message).ConfigureAwait(false);
+        }
+    }
+}
---
+++ CommunicationService/CommunicationService.MessageService/InterUserMessage.cs
@@ -0,0 +1,166 @@
+﻿using CommunicationService.Core.Domains;
+using CommunicationService.Core.Interfaces;
+using CommunicationService.Core.Interfaces.Repositories;
+using CommunicationService.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HelpMyStreet.Utils.Models;
+using CommunicationService.MessageService.Substitution;
+using System.Globalization;
+using Polly.Caching;
+using HelpMyStreet.Utils.Enums;
+using HelpMyStreet.Utils.Extensions;
+
+namespace CommunicationService.MessageService
+{
+    public class InterUserMessage : IMessage
+    {
+        private readonly IConnectRequestService _connectRequestService;
+        private readonly IConnectUserService _connectUserService;
+        private const string EMAIL_TO_ADDRESS = "EmailToAddress";
+        private const string EMAIL_TO_NAME = "EmailToName";
+        private const string RECIPIENT_FIRST_NAME = "RecipientFirstName";
+
+        List<SendMessageRequest> _sendMessageRequests;
+
+        public string UnsubscriptionGroupName
+        {
+            get
+            {
+                return UnsubscribeGroupName.InterUserMessage;
+            }
+        }
+
+        public InterUserMessage(IConnectRequestService connectRequestService, IConnectUserService connectUserService)
+        {
+            _connectRequestService = connectRequestService;
+            _connectUserService = connectUserService;
+            _sendMessageRequests = new List<SendMessageRequest>();
+        }
+
+        public async Task<Dictionary<string,string>> GetRecipientDetails(int? recipientUserId, Dictionary<string, string> additionalParameters)
+        {
+            var result = new Dictionary<string, string>();
+
+            var user = await _connectUserService.GetUserByIdAsync(recipientUserId.Value);
+
+            if(user!=null)
+            {
+                result.Add(EMAIL_TO_ADDRESS, user.UserPersonalDetails.EmailAddress);
+                result.Add(EMAIL_TO_NAME, $"{user.UserPersonalDetails.FirstName} {user.UserPersonalDetails.LastName}");
+                result.Add(RECIPIENT_FIRST_NAME, user.UserPersonalDetails.FirstName);
+                return result;
+            }
+
+            if(additionalParameters.TryGetValue("RecipientEmailAddress", out string emailAddress) 
+                && additionalParameters.TryGetValue("RecipientDisplayName", out string displayName))
+            {
+                result.Add(EMAIL_TO_ADDRESS, emailAddress);
+                result.Add(EMAIL_TO_NAME, displayName);
+                result.Add(RECIPIENT_FIRST_NAME, displayName);
+                return result;
+            }
+
+            throw new Exception("Unable to Get Recipient details");
+        }
+
+        private async Task<string> SenderAndContext(string senderName, string fromRequestorRole, int? jobId)
+        {
+            string result = string.Empty;
+            RequestRoles requestRole = (RequestRoles)Enum.Parse(typeof(RequestRoles), fromRequestorRole);
+            switch(requestRole)
+            {
+                case RequestRoles.Recipient:
+                    result = $"{senderName}, the person you helped.";
+                    break;
+                case RequestRoles.Requestor:
+                    result = $"{senderName}, the person you requested help for.";
+                    break;
+                case RequestRoles.Volunteer:
+                    result = $"{senderName}, the person who requested help for you.";
+                    break;
+                default:
+                    break;
+            }
+
+            if(jobId.HasValue)
+            {
+                var job = await _connectRequestService.GetJobDetailsAsync(jobId.Value);
+                if (job != null)
+                {
+                    DateTime dtStatusChanged = job.JobSummary.DateStatusLastChanged;
+                    result = $"{result} The request was for { Mapping.ActivityMappings[job.JobSummary.SupportActivity]} and was {Mapping.StatusMappingsNotifications[job.JobSummary.JobStatus]} {dtStatusChanged.FriendlyPastDate()}";
+                }
+            }
+            return result;
+        }
+
+        public async Task<EmailBuildData> PrepareTemplateData(Guid batchId, int? recipientUserId, int? jobId, int? groupId, Dictionary<string, string> additionalParameters, string templateName)
+        {
+            if (!recipientUserId.HasValue || !jobId.HasValue)
+            {
+                throw new Exception($"Recipient or JobID is missing");
+            }
+
+            if (recipientUserId.Value > 0)
+            {
+                var user = await _connectUserService.GetUserByIdAsync(recipientUserId.Value);
+            }
+            var recipientDetails = await GetRecipientDetails(recipientUserId, additionalParameters);
+
+            string subject = "A personal message sent through HelpMyStreet";
+            string title = "Personal message received";
+            string recipientFirstName = string.Empty;
+            string senderFirstName = string.Empty;
+            additionalParameters.TryGetValue("FromRequestorRole", out string fromRequestorRole);            
+            string senderMessage = string.Empty;
+            string emailToAddress = string.Empty;
+            string emailToName = string.Empty;            
+
+            if(recipientDetails!=null)
+            {
+                emailToAddress = recipientDetails[EMAIL_TO_ADDRESS];
+                emailToName = recipientDetails[EMAIL_TO_NAME];
+                recipientFirstName = recipientDetails[RECIPIENT_FIRST_NAME];
+            }
+
+            if (additionalParameters != null)
+            {
+                additionalParameters.TryGetValue("SenderMessage", out senderMessage);
+                additionalParameters.TryGetValue("SenderFirstName", out senderFirstName);               
+            }
+            string senderAndContext = await SenderAndContext( senderFirstName, fromRequestorRole, jobId);
+
+            return new EmailBuildData()
+            {
+                BaseDynamicData = new InterUserMessageData(
+                    title,
+                    subject,
+                    recipientFirstName,
+                    senderAndContext,
+                    senderFirstName,
+                    senderMessage
+                    ),
+                EmailToAddress = emailToAddress,
+                EmailToName = emailToName
+            };
+        }
+
+        private void AddRecipientAndTemplate(string templateName, int userId, int? jobId, int? groupId)
+        {
+            _sendMessageRequests.Add(new SendMessageRequest()
+            {
+                TemplateName = templateName,
+                RecipientUserID = userId,
+                JobID = jobId,
+                GroupID = groupId
+            });
+        }
+
+        public async Task<List<SendMessageRequest>> IdentifyRecipients(int? recipientUserId, int? jobId, int? groupId)
+        {
+            return null;
+        }
+    }
+}
