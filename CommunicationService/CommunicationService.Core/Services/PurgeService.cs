d61603346b63a1df48d592eff476fbdae5d0abe4
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-09-30 09:31:33 +0100

    add link expiry

---
+++ CommunicationService/CommunicationService.Core/Services/PurgeService.cs
@@ -0,0 +1,32 @@
+﻿using CommunicationService.Core.Domains;
+using CommunicationService.Core.Interfaces.Repositories;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunicationService.Core.Services
+{
+    public class PurgeService : IPurgeService
+    {
+        private readonly ILinkRepository _linkRepository;
+
+        public PurgeService(ILinkRepository linkRepository)
+        {
+            _linkRepository = linkRepository;
+        }
+
+        public async Task PurgeExpiredLinks()
+        {
+            var links = await _linkRepository.GetExpiredLinksAsync();
+
+            if(links!=null && links.Count()>0)
+            {
+                foreach (Links l in links)
+                {
+                    await _linkRepository.DeleteLink(l.id);
+
+                }
+            }
+        }
+    }
+}
