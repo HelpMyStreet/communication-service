14ed851856524c1d80a8d9fafbf27116714b85a5
Mike Matthews <mike@factor-50.co.uk>
2020-12-03 16:43:48 +0000

    FE-557 FE-558 Splitting RequestorTaskConfirmation from TaskNotification

---
+++ CommunicationService/CommunicationService.MessageService/Substitution/RequestorTaskConfirmationData.cs
@@ -0,0 +1,26 @@
+﻿
+using CommunicationService.Core.Domains;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunicationService.MessageService.Substitution
+{
+    public class RequestorTaskConfirmationData : BaseDynamicData
+    {
+        public string FirstName { get; private set; }
+        public bool StatusIsOpen { get; private set; }
+        public string GroupName { get; private set; }
+
+        public RequestorTaskConfirmationData(
+            string firstname,
+            bool statusIsOpen,
+            string groupName
+            )
+        {
+            FirstName = firstname;
+            StatusIsOpen = statusIsOpen;
+            GroupName = groupName;
+        }
+    }
+}
