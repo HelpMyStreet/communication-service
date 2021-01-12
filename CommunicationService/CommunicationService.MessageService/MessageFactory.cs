2e7bacd5f05865ff8f15b94523a7ded289501d8b
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-01-12 14:07:48 +0000

    add case to message factory

--- CommunicationService/CommunicationService.MessageService/MessageFactory.cs
+++ CommunicationService/CommunicationService.MessageService/MessageFactory.cs
@@ -74,6 +74,8 @@ namespace CommunicationService.MessageService
                     return new RegistrationChaserMessage(_connectUserService, _cosmosDbService,_emailConfig);
                 case CommunicationJobTypes.SendNewTaskNotification:
                     return new TaskNotificationMessage(_connectUserService, _connectRequestService, _connectGroupService);
+                case CommunicationJobTypes.RequestorTaskConfirmation:
+                    return new RequestorTaskConfirmation(_connectRequestService, _connectGroupService);
                 case CommunicationJobTypes.SendTaskStateChangeUpdate:
                     //return new TaskUpdateNewMessage(_connectRequestService, _connectUserService, _connectGroupService, _linkRepository, _linkConfig, _sendGridConfig);
                     return new TaskUpdateSimplifiedMessage(_connectRequestService, _connectUserService, _connectGroupService, _linkRepository, _linkConfig, _sendGridConfig);
