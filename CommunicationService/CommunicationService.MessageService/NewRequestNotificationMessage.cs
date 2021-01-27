f967346f75798f762b6b39fcff718bd4da4efe62
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-01-27 11:38:57 +0000

    Add identify recipients

--- CommunicationService/CommunicationService.MessageService/NewRequestNotificationMessage.cs
+++ CommunicationService/CommunicationService.MessageService/NewRequestNotificationMessage.cs
@@ -1,15 +1,25 @@
 ﻿using CommunicationService.Core.Domains;
 using CommunicationService.Core.Interfaces;
+using CommunicationService.Core.Interfaces.Services;
 using CommunicationService.MessageService.Substitution;
+using HelpMyStreet.Contracts.RequestService.Request;
+using HelpMyStreet.Utils.Enums;
+using HelpMyStreet.Utils.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CommunicationService.MessageService
 {
     public class NewRequestNotificationMessage : IMessage
     {
+        private readonly IConnectRequestService _connectRequestService;
+        private readonly IConnectAddressService _connectAddressService;
+        private readonly IConnectUserService _connectUserService;
         List<SendMessageRequest> _sendMessageRequests;
 
         public string GetUnsubscriptionGroupName(int? recipientUserId)
@@ -17,14 +27,59 @@ namespace CommunicationService.MessageService
             return UnsubscribeGroupName.TaskNotification;
         }
 
-        public NewRequestNotificationMessage()
+        public NewRequestNotificationMessage(IConnectRequestService connectRequestService, IConnectAddressService connectAddressService, IConnectUserService connectUserService)
         {
+            _connectRequestService = connectRequestService;
+            _connectAddressService = connectAddressService;
+            _connectUserService = connectUserService;
             _sendMessageRequests = new List<SendMessageRequest>();
         }
 
         public async Task<List<SendMessageRequest>> IdentifyRecipients(int? recipientUserId, int? jobId, int? groupId, int? requestId, Dictionary<string, string> additionalParameters)
         {
-            AddRecipientAndTemplate(TemplateName.RequestNotification, recipientUserId.Value, jobId, groupId, requestId, additionalParameters);
+            GetOpenShiftJobsByFilterRequest request = new GetOpenShiftJobsByFilterRequest();
+            var shifts = await _connectRequestService.GetOpenShiftJobsByFilter(request);
+
+            List<int> usersToBeNotified = new List<int>();
+
+            if(shifts.ShiftJobs.Count>0)
+            {         
+                var locationsSupportActivities = shifts.ShiftJobs.GroupBy(d => new { d.Location, d.SupportActivity })
+                    .Select(m => new { m.Key.Location, m.Key.SupportActivity });
+
+                if(locationsSupportActivities!=null && locationsSupportActivities.Count()>0)
+                {
+                    foreach(var x in locationsSupportActivities)
+                    {
+                        var locations = await _connectAddressService.GetLocationDetails(x.Location);
+
+                        if(locations !=null)
+                        {
+                            var users = await _connectUserService.GetVolunteersByPostcodeAndActivity(
+                                locations.LocationDetails.Address.Postcode,
+                                new List<SupportActivities>() { x.SupportActivity },
+                                CancellationToken.None);
+
+                            if(users!=null && users.Volunteers.Count()>0)
+                            {
+                                usersToBeNotified.AddRange(users.Volunteers.Select(x => x.UserID).ToList());
+                            }
+                        }
+                    }                    
+                }
+
+                if(usersToBeNotified.Count>0)
+                {
+                    usersToBeNotified
+                        .Distinct()
+                        .ToList()
+                        .ForEach(userId =>
+                        {
+                            AddRecipientAndTemplate(TemplateName.RequestNotification, userId, null, null, null, additionalParameters);
+                        });
+                }
+            }
+            
             return _sendMessageRequests;
         }
 
