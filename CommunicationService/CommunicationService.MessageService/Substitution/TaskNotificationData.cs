b40781f6ac3b12a1bbd418956a1d0782a696d159
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-02-22 15:49:06 +0000

    Tweak notfication email to be driven of request id

--- CommunicationService/CommunicationService.MessageService/Substitution/TaskNotificationData.cs
+++ CommunicationService/CommunicationService.MessageService/Substitution/TaskNotificationData.cs
@@ -9,35 +9,38 @@ namespace CommunicationService.MessageService.Substitution
     {
         public string FirstName { get; private set; }
         public bool IsRequestor { get; private set; }
-        public string EncodedJobID { get; private set; }
+        public string EncodedRequestID { get; private set; }
         public string Activity { get; private set; }
         public string PostCode { get; private set; }
         public double DistanceFromPostCode { get; private set; }
         public string DueDate { get; private set; }
         public bool IsHealthCritical { get; private set; }
         public bool IsFaceMask { get; private set; }
+        public string NumberOfVolunteers { get; set; }
 
         public TaskNotificationData(
             string firstname,
             bool isRequestor,
-            string encodedJobID,
+            string encodedRequestID,
             string activity,
             string postcode,
             double distanceFromPostcode,
             string dueDate,
             bool isHealthCritical,
-            bool isFaceMask
+            bool isFaceMask,
+            string numberofVolunteers
             )
         {
             FirstName = firstname;
             IsRequestor = isRequestor;
-            EncodedJobID = encodedJobID;
+            EncodedRequestID = encodedRequestID;
             Activity = activity;
             PostCode = postcode;
             DistanceFromPostCode = distanceFromPostcode;
             DueDate = dueDate;
             IsHealthCritical = isHealthCritical;
             IsFaceMask = isFaceMask;
+            NumberOfVolunteers = numberofVolunteers;
         }
     }
 }
