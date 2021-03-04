93a0503db4bc59996a13c8f746b1518f36870e71
Mike Matthews <mike@factor-50.co.uk>
2021-03-04 14:44:00 +0000

    FE-557 Correcting parameter name StatusIsOpen --> PendingApproval

--- CommunicationService/CommunicationService.MessageService/Substitution/RequestorTaskConfirmationData.cs
+++ CommunicationService/CommunicationService.MessageService/Substitution/RequestorTaskConfirmationData.cs
@@ -33,18 +33,18 @@ namespace CommunicationService.MessageService.Substitution
     public class RequestorTaskConfirmationData : BaseDynamicData
     {
         public string FirstName { get; private set; }
-        public bool StatusIsOpen { get; private set; }
+        public bool PendingApproval { get; private set; }
         public string GroupName { get; private set; }
         public List<RequestJob> RequestJobList { get; private set; }
         public RequestorTaskConfirmationData(
             string firstname,
-            bool statusIsOpen,
+            bool pendingApproval,
             string groupName,
             List<RequestJob> requestJobList
             )
         {
             FirstName = firstname;
-            StatusIsOpen = statusIsOpen;
+            PendingApproval = pendingApproval;
             GroupName = groupName;
             RequestJobList = requestJobList;
         }
