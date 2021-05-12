9955ea6b6bf8d03fa4c7df86f801f905aade163e
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-05-12 11:04:50 +0100

    rename cache method

--- CommunicationService/CommunicationService.Core/Interfaces/Services/IConnectSendGridService.cs
+++ CommunicationService/CommunicationService.Core/Interfaces/Services/IConnectSendGridService.cs
@@ -10,7 +10,7 @@ namespace CommunicationService.Core.Interfaces.Services
     public interface IConnectSendGridService
     {
         Task<Template> GetTemplate(string templateName);
-        Task<Template> GetTemplate(string templateName, CancellationToken cancellationToken);
+        Task<Template> GetTemplateWithCache(string templateName, CancellationToken cancellationToken);
         Task<bool> SendDynamicEmail(string messageId, string templateName, string groupName, EmailBuildData sendGridData);
         Task<int> GetGroupId(string groupName);
         Task<bool> AddNewMarketingContact(MarketingContact marketingContact);
